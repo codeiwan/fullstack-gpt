8665e5377791fc080a3d11e44d2cdb8c1cf483ef
codeiwan <pewlv7976@gmail.com>
2025-07-27 23:27:34 +0900

    DocumentGPT-Data Flow

--- Home.py
+++ Home.py
@@ -1,16 +1,30 @@
 import streamlit as st
-from langchain.prompts import PromptTemplate
+from datetime import datetime
 
-st.write("hello")
+today = datetime.today().strftime("%H:%M:%S")
 
-a = [1, 2, 3, 4]
+st.title(today)
 
-d = {'x': 1}
+model = st.selectbox(
+    "Choose your model",
+    (
+        "GPT-3",
+        "GPT-4"
+    ),
+)
 
-p = PromptTemplate.from_template("xxxx")
+if model == "GPT-3":
+    st.write("cheap")
+else:
+    st.write("not cheap")
 
-a
+    name = st.text_input("What is your name?")
+    st.write(name)
 
-d
+    value = st.slider(
+        "temperature",
+        min_value=0.1,
+        max_value=1.0,
+    )
 
-st.selectbox("Choose your model", ("GPT-3", "GPT-4"))
+    st.write(value)
